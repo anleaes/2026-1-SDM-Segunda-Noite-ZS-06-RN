c15cca8a27e995406f41b620d6bded02f9262987
Pedro Barison <pedrobarison@gmail.com>
2026-05-30 23:27:32 -0300

    Criação de GameList.jsx em pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <h1>A3 Biblioteca de Jogos Antigos</h1>
+      <p>Frontend React funcionando!</p>
     </div>
   );
 }
---
+++ src/pages/GameList.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
+function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        api.get("/jogos/game/")
+            .then((response) => {
+                setGames(response.data);
+            })
+            .catch((error) => {
+                console.error("Erro ao carregar jogos:", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Biblioteca de Jogos Antigos</h1>
+
+            <ul>
+                {games.map((game) => (
+                    <li key={game.id}>
+                        {game.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default GameList;
