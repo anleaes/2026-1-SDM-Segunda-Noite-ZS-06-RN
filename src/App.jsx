342c953ba7e7a21d2a57e8c28bf92f4b51d93081
Pedro Barison <pedrobarison@gmail.com>
2026-05-31 21:43:22 -0300

    Front do GameList funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import GameList from "./pages/GameList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Games from "./pages/Games";
+import Login from "./pages/Login";
 
 function App() {
     return (
